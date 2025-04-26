26092dffcf01fb50f8fa6ee365a4ca40910b9071
Sourav chouhan <souravchouhanvk@gmail.com>
2025-04-26 17:20:09 +0530

    Handle single number input

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,7 +1,9 @@
 class StringCalculator
-	def add(numbers)
-		0
-	end
+  def add(numbers)
+    return 0 if numbers.empty?
+
+    numbers.to_i
+  end
 end
 
-# Ruby file of implementation for empty string
+# Ruby file of implementation to handle single number input
--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -2,10 +2,10 @@ require_relative 'string_calculator'
 
 RSpec.describe StringCalculator do
   describe "#add" do
-    it "returns 0 for empty string" do
-      expect(StringCalculator.new.add("")).to eq(0)
+    it "returns the number itself for a single number" do
+        expect(StringCalculator.new.add("1")).to eq(1)
     end
   end
 end
 
-# Rspec test case to check return 0 for empty string.
+# Rspec test case to handle single number input.
